6efe9c329422aa167c306a9d44252c3f4757b1c5
Alex Walters <alexnwalters@gmail.com>
2019-05-28 23:56:35 -0400

    create server with express & morgan for google apps

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const playstore = require('./playstore')
+
+
+app.use(morgan('common'));
+
+app.get('/apps', (req, res) => {
+
+    let genresOptions = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+    let sortOptions = ['Rating', 'App'];
+
+    const { sort, genres } = req.query;
+
+    if(sort) {
+        if(!sortOptions
+            .toString()
+            .toLowerCase()
+            .includes(sort.toLowerCase())) {
+                return res
+                    .status(400)
+                    .send('Sort must be one of rating or app');
+        }
+    }
+
+    if(genres) {
+        if(!genresOptions
+            .toString()
+            .toLowerCase()
+            .includes(genres.toLowerCase())) {
+                return res
+                    .status(400)
+                    .send(`Genres must be one of the following options: ${genresOptions}.`)
+        }
+    }
+
+    let results = playstore
+                    .filter(item =>
+                        item
+                        .Genres
+                        .toLowerCase()
+                        .includes(genres.toLowerCase())
+                        );
+
+    if(sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        })
+    }
+
+    res
+        .json(results);
+});
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+});
